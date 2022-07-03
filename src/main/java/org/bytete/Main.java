b4d8079976b01ccf1958a9975b48c3ae10ed198b
简尚全 <jianshangquan3@gmail.com>
2022-07-04 00:36:39 +0630

    update sample

--- src/main/java/org/bytete/Main.java
+++ src/main/java/org/bytete/Main.java
@@ -2,6 +2,7 @@ package org.bytete;
 
 import org.bytete.Callbacks.ParameterReturnTypeCallback;
 import org.bytete.Callbacks.ParameterVoidCallback;
+import org.bytete.Callbacks.ReturnTypeCallback;
 
 public class Main{
     public static void main(String args[]) throws InterruptedException {
@@ -10,17 +11,21 @@ public class Main{
         Async async = new Async(() -> {
             int money = 10;
             return money;
-        }).then(() -> {
+        }).then((ParameterReturnTypeCallback<Integer, Integer>) (Integer data) -> {
             System.out.println("do next step");
+            return data + 20;
         }).then((ParameterReturnTypeCallback<Integer, String>) (Integer data) -> {
             System.out.println(data);
-            return "lastValue";
+            return data + " lastValue";
         }).then((ParameterVoidCallback<String>) (String data) -> {
             System.out.println(data);
+        }).then(() -> {
+            System.out.println("do next step");
         }).catchError((e) -> {
             System.out.println(e);
+        }).handleFinally(() -> {
+            System.out.println("clean codes");
         });
-
 
         async.async();
  
